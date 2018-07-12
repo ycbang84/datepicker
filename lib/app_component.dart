956f1a1c030a5b55fb02aab0a985ff05b116f6bc
DartLover <ycbang84@live.com>
2018-07-11 22:37:00 -0700

    test
    test

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,5 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/todo_list_component.dart';
 import 'dart:convert';
 import 'dart:html';
 
@@ -9,45 +8,16 @@ import 'dart:html';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+
 
   AppComponent() {
-    TestData temp = new TestData();
-    temp.date = "bang!!";
-    temp.indexs = [];
-    temp.indexs.add(1);
-    temp.indexs.add(10);
-    temp.cd = new ChildDate();
-    temp.cd.id = "test-12";
-    temp.cd.name = "make money!!";
-
-    window.console.log(temp.toJson());
   }
-
   
 
 }
 
 
-
-class ChildDate{
-  String id;
-  String name;
-}
-
-
-class TestData {
-  String date;
-  List<int> indexs;
-  ChildDate cd;
-
-  String toJson() => jsonEncode(this, toEncodable: (value) {
-    if(value is Object) {
-      
-    }
-  });
-}
 
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'package:angular/angular.dart';
-import 'package:testUi/app_component.template.dart' as ng;
+import 'package:DatePicker/app_component.template.dart' as ng;
 
 void main() {
   runApp(ng.AppComponentNgFactory);
